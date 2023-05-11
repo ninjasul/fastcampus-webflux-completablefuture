dd11ae66d8c814fb20dd826eeab5d89cbfbda1e9
dongyub.park <dongyub.park@kurlycorp.com>
2023-05-12 07:27:27 +0900

    feat: UserFutureService 개발

--- src/main/java/com/ninjasul/completablefuture/future/service/UserFutureService.java
+++ src/main/java/com/ninjasul/completablefuture/future/service/UserFutureService.java
@@ -3,11 +3,13 @@ package com.ninjasul.completablefuture.future.service;
 import com.ninjasul.completablefuture.common.Article;
 import com.ninjasul.completablefuture.common.Image;
 import com.ninjasul.completablefuture.common.User;
+import com.ninjasul.completablefuture.common.entity.UserEntity;
 import com.ninjasul.completablefuture.future.repository.ArticleFutureRepository;
 import com.ninjasul.completablefuture.future.repository.FollowFutureRepository;
 import com.ninjasul.completablefuture.future.repository.ImageFutureRepository;
 import com.ninjasul.completablefuture.future.repository.UserFutureRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,34 +21,38 @@ public class UserFutureService {
     private final ImageFutureRepository imageRepository;
     private final FollowFutureRepository followRepository;
 
+    @SneakyThrows
     public Optional<User> getUserById(String id) {
-        return userRepository.findById(id)
-            .map(user -> {
-                var image = imageRepository.findById(user.getProfileImageId())
-                    .map(imageEntity -> new Image(imageEntity.getId(), imageEntity.getName(), imageEntity.getUrl()));
+        return userRepository.findById(id).get()
+            .map(this::getUser);
+    }
 
-                var articles = articleRepository.findAllByUserId(user.getId())
-                    .stream()
-                    .map(articleEntity ->
-                            new Article(
-                                articleEntity.getId(),
-                                articleEntity.getTitle(),
-                                articleEntity.getContent(),
-                                articleEntity.getUserId()
-                            )
-                    )
-                    .collect(Collectors.toList());
+    @SneakyThrows
+    private User getUser(UserEntity user) {
+        var image = imageRepository.findById(user.getProfileImageId()).get()
+            .map(imageEntity -> new Image(imageEntity.getId(), imageEntity.getName(), imageEntity.getUrl()));
 
-                var followCount = followRepository.countByUserId(user.getId());
+        var articles = articleRepository.findAllByUserId(user.getId()).get()
+            .stream()
+            .map(articleEntity ->
+                new Article(
+                    articleEntity.getId(),
+                    articleEntity.getTitle(),
+                    articleEntity.getContent(),
+                    articleEntity.getUserId()
+                )
+            )
+            .collect(Collectors.toList());
 
-                return new User(
-                    user.getId(),
-                    user.getName(),
-                    user.getAge(),
-                    image,
-                    articles,
-                    followCount
-                );
-            });
+        var followCount = followRepository.countByUserId(user.getId()).get();
+
+        return new User(
+            user.getId(),
+            user.getName(),
+            user.getAge(),
+            image,
+            articles,
+            followCount
+        );
     }
 }
--- src/test/java/com/ninjasul/completablefuture/blocking/service/future/UserServiceFutureTest.java
+++ src/test/java/com/ninjasul/completablefuture/blocking/service/future/UserServiceFutureTest.java
@@ -1,32 +1,41 @@
-package com.ninjasul.completablefuture.blocking.service;
+
+package com.ninjasul.completablefuture.blocking.service.future;
 
 import com.ninjasul.completablefuture.blocking.repository.ArticleRepository;
 import com.ninjasul.completablefuture.blocking.repository.FollowRepository;
 import com.ninjasul.completablefuture.blocking.repository.ImageRepository;
 import com.ninjasul.completablefuture.blocking.repository.UserRepository;
+import com.ninjasul.completablefuture.blocking.service.UserBlockingService;
 import com.ninjasul.completablefuture.common.User;
+import com.ninjasul.completablefuture.future.repository.ArticleFutureRepository;
+import com.ninjasul.completablefuture.future.repository.FollowFutureRepository;
+import com.ninjasul.completablefuture.future.repository.ImageFutureRepository;
+import com.ninjasul.completablefuture.future.repository.UserFutureRepository;
+import com.ninjasul.completablefuture.future.service.UserFutureService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class UserBlockingServiceTest {
-    UserBlockingService userBlockingService;
-    UserRepository userRepository;
-    ArticleRepository articleRepository;
-    ImageRepository imageRepository;
-    FollowRepository followRepository;
+class UserServiceFutureTest {
+    UserFutureService userFutureService;
+    UserFutureRepository userRepository;
+    ArticleFutureRepository articleRepository;
+    ImageFutureRepository imageRepository;
+    FollowFutureRepository followRepository;
 
     @BeforeEach
     void setUp() {
-        userRepository = new UserRepository();
-        articleRepository = new ArticleRepository();
-        imageRepository = new ImageRepository();
-        followRepository = new FollowRepository();
+        userRepository = new UserFutureRepository();
+        articleRepository = new ArticleFutureRepository();
+        imageRepository = new ImageFutureRepository();
+        followRepository = new FollowFutureRepository();
 
-        userBlockingService = new UserBlockingService(
+        userFutureService = new UserFutureService(
             userRepository,
             articleRepository,
             imageRepository,
@@ -40,7 +49,7 @@ class UserBlockingServiceTest {
         String userId = "invalid_user_id";
 
         // when
-        Optional<User> user = userBlockingService.getUserById(userId);
+        Optional<User> user = userFutureService.getUserById(userId);
 
         // then
         assertTrue(user.isEmpty());
@@ -52,7 +61,7 @@ class UserBlockingServiceTest {
         String userId = "1234";
 
         // when
-        Optional<User> optionalUser = userBlockingService.getUserById(userId);
+        Optional<User> optionalUser = userFutureService.getUserById(userId);
 
         // then
         assertFalse(optionalUser.isEmpty());
